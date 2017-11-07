9be4421ca00570945464a86d8e24c362e709fdab
Karolina <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-11-07 16:56:14 +0100

    Update tree.js

--- tree.js
+++ tree.js
@@ -21,3 +21,28 @@ function drawTree(n) {
     
 }
 drawTree(10)
+
+//beautifull christmas tree
+function drawTree(n) {
+ 
+//poziomy
+    for (var i = 1; i <= n; i++) {
+        star = '*';
+//iteracje
+        for (var j = n - 1; j > i; j--) {
+            star = star + '    ';
+        }
+        for (var j = 1; j <= (3 * i - 1); j++) {
+
+            star = star += '{*}';
+        }
+       console.log(star);
+        
+        if (i==9) {
+            console.log('  **Merry Christmas****Merry Christmas****Merry Christmas****Merry Christmas**');
+        }
+        continue;
+    }
+    
+}
+drawTree(10)
