ccf9c9a93f131f469c6091b7cb830f4fbbc82fcb
Karolina <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-11-07 16:44:49 +0100

    Create tree.js

---
+++ tree.js
@@ -0,0 +1,23 @@
+//rysowanie choinki za pomocą  console.log
+function drawTree(n) {
+//poziomy
+    for (var i = 1; i <= n; i++) {
+        star = '';
+//iteracje
+        for (var j = n - 1; j > i; j--) {
+            star = star + ' ';
+        }
+        for (var j = 1; j <= (2 * i - 1); j++) {
+
+            star = star += '*';
+        }
+       console.log(star);
+        
+        if (i==9) {
+            console.log('**Merry Christmas**');
+        }
+        continue;
+    }
+    
+}
+drawTree(10)
