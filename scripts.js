f424f1a24959ef95a53166c343eab9b723ab27fc
Karolina <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-11-11 08:24:42 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,10 @@ var list = document.getElementById('list'),
     add = document.getElementById('addElem');
 
 add.addEventListener('click', function () {
-    'use strict';
-     var element = document.createElement('li'),
-        num = document.getElementsByTagName('li');
-
-    element.innerHTML = 'Item' + num.length;
-    list.appendChild(element);
-
+    var element = document.createElement('li'),
+            num = document.getElementsByTagName('li');
+    
+    element.innerHTML = 'Item' + ' '+ num.length;
+    list.appendChild(element); 
+    
 });
